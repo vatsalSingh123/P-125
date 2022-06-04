a0bfc5872565c772e82e92bc72b797a424b3b477
vatsalSingh123 <99068288+vatsalSingh123@users.noreply.github.com>
2022-06-04 09:18:38 +0530

    Add files via upload

---
+++ classifier.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from PIL import Image
+import PIL.ImageOps
+
+# FETCHING THE DATA
+X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
+
+#SPLITTING OF THE DATA
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=9, train_size=3500, 
+                                                    test_size=500)
+#scaling the features
+X_train_scaled = X_train/255.0
+X_test_scaled = X_test/255.0
+
+#Fitting the training data into the model
+clf = LogisticRegression(solver='saga', multi_class='multinomial').fit(X_train_scaled, y_train)
+
+def get_prediction(image):
+    im_pil = Image.open(image)
+    image_bw = im_pil.convert('L')
+    image_bw_resized = image_bw.resize((22,30), Image.ANTIALIAS)
+    pixel_filter = 20
+    min_pixel = np.percentile(image_bw_resized, pixel_filter)
+    image_bw_resized_inverted_scaled = np.clip(image_bw_resized-min_pixel, 0, 255)
+    max_pixel = np.max(image_bw_resized)
+    image_bw_resized_inverted_scaled = np.asarray(image_bw_resized_inverted_scaled)/max_pixel
+    test_sample = np.array(image_bw_resized_inverted_scaled).reshape(1,784)
+    test_pred = clf.predict(test_sample)
+    return test_pred[0]
+
+
