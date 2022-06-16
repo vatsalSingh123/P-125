31e6708bb7ddb6032fca367b597c13ef4055ba85
vatsalSingh123 <99068288+vatsalSingh123@users.noreply.github.com>
2022-06-16 09:36:34 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask,jsonify,request
-from classifier import get_prediction
+from flask import Flask, jsonify, request
+from classifier import  get_prediction
 
 app = Flask(__name__)
 
-@app.route("/pred-digit",methods=['POST'])
+@app.route("/predict-alphabet", methods=["POST"])
 def predict_data():
-    img = request.files.get("digit")
-    prediction = get_prediction(img)
-    return jsonify({
-    "prediction":prediction
-    }),200
-    
+  image = request.files.get("alphabet")
+  prediction = get_prediction(image)
+  return jsonify({
+    "prediction": prediction
+  }), 200
+
 if __name__ == "__main__":
-    app.run(debug=True)
+  app.run(debug=True)
--- classifier.py
+++ classifier.py
@@ -1,3 +1,4 @@
+import cv2
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -8,18 +9,18 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from PIL import Image
 import PIL.ImageOps
+import os, ssl, time
 
-# FETCHING THE DATA
-X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
+X = np.load('image.npz')['arr_0']
+y = pd.read_csv("labels.csv")["labels"]
+print(pd.Series(y).value_counts())
+classes = ['A', 'B', 'C', 'D', 'E','F', 'G', 'H', 'I', 'J', "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+nclasses = len(classes)
 
-#SPLITTING OF THE DATA
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=9, train_size=3500, 
-                                                    test_size=500)
-#scaling the features
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=9, train_size=3500, test_size=500)
 X_train_scaled = X_train/255.0
 X_test_scaled = X_test/255.0
 
-#Fitting the training data into the model
 clf = LogisticRegression(solver='saga', multi_class='multinomial').fit(X_train_scaled, y_train)
 
 def get_prediction(image):
@@ -31,8 +32,6 @@ def get_prediction(image):
     image_bw_resized_inverted_scaled = np.clip(image_bw_resized-min_pixel, 0, 255)
     max_pixel = np.max(image_bw_resized)
     image_bw_resized_inverted_scaled = np.asarray(image_bw_resized_inverted_scaled)/max_pixel
-    test_sample = np.array(image_bw_resized_inverted_scaled).reshape(1,784)
+    test_sample = np.array(image_bw_resized_inverted_scaled).reshape(1,660)
     test_pred = clf.predict(test_sample)
     return test_pred[0]
-
-
